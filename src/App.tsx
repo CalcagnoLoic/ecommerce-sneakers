d0c1b1f8ac7055fcffc805acfbef89ee0604e367
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2023-12-19 16:29:27 +0100

    adding header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { Header } from "./components/common/Header";
+import { Footer } from "./components/common/Footer";
+
 export const App: React.FC = () => {
-  return <></>;
+  return (
+    <div className="flex flex-col justify-center h-screen">
+      <header>
+        <Header />
+      </header>
+
+      <main className="m-auto"></main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div >
+  );
 };
---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer: React.FC = () => {
+  return (
+    <>
+      <p className="text-center py-5">Coded with love by <a href="https://github.com/CalcagnoLoic">@CalcagnoLoic</a></p>
+    </>
+  );
+};
