d8066628ee8b88d81401db977f8d09a22cb5a9ac
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2023-12-19 15:42:31 +0100

    starting the project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        pumpkin: "#FF7D1A",
+        peachCream: "#FFEDE0",
+        shark: "#1D2025",
+        paleSky: "#68707D",
+        cadetBlue: "#B6BCC8",
+        linkWater: "#F7F8FD",
+        white: "#FFFFFF",
+        dark: "#000000"
+      },
+    },
+  },
+  plugins: [],
+};
