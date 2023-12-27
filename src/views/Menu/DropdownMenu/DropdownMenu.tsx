4bc81e1ade8cc5faa7603cda733f07d65f074089
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2023-12-27 10:57:34 +0100

    adding effects on sidebar

--- src/views/Menu/DropdownMenu/DropdownMenu.tsx
+++ src/views/Menu/DropdownMenu/DropdownMenu.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { names_navbar, NavBar } from "../../../data/data.js";
 
 import MENU_ICON from "/assets/img/icon-menu.svg";
@@ -7,34 +7,46 @@ import LOGO from "/assets/img/logo.svg";
 
 export const DropdownMenu = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const sidebarRef = useRef<HTMLDivElement>(null);
+
+  const handleClickClose = (): void => {
+    setIsOpen(true);
+  };
 
   useEffect(() => {
-    const handleClickOutside = (e: MouseEvent): void => {
-      const sidebar = document.querySelector(".sidebar");
+    const handleBodyOverflow = (shouldOverflow: boolean): void => {
+      document.body.classList.toggle("no-scroll", shouldOverflow);
+    };
 
-      if (!isOpen && sidebar && !sidebar.contains(e.target as Node)) {
+    const handleClickOutside = (event: MouseEvent): void => {
+      if (
+        isOpen &&
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target as Node)
+      ) {
         setIsOpen(false);
       }
     };
 
-    if (isOpen) {
-      document.body.style.overflow = "hidden";
-      document.addEventListener("click", handleClickOutside);
-    } else {
-      document.body.style.overflow = "visible";
-      document.removeEventListener("click", handleClickOutside);
-    }
+    isOpen ? handleBodyOverflow(true) : handleBodyOverflow(false);
+    isOpen
+      ? document.body.addEventListener("mousedown", handleClickOutside)
+      : document.body.removeEventListener("mousedown", handleClickOutside);
+
     return (): void => {
-      document.removeEventListener("click", handleClickOutside);
+      handleBodyOverflow(false);
+      document.body.removeEventListener("mousedown", handleClickOutside);
     };
   }, [isOpen]);
-
 
   return (
     <>
       <div className={`${isOpen ? "sidebar relative lg:hidden" : "hidden"}`}>
         <div className="wrapper-navbar w-screen bg-black opacity-80"></div>
-        <div className="wrapper-navbar ml-[-24px] flex flex-col gap-5 bg-white pl-7 pr-24 pt-28">
+        <div
+          className="wrapper-navbar ml-[-24px] flex flex-col gap-5 bg-white pl-7 pr-24 pt-28"
+          ref={sidebarRef}
+        >
           {names_navbar.map((elem: NavBar) => (
             <a href="#" className="link-nav" key={elem.id}>
               {elem.value}
@@ -44,13 +56,13 @@ export const DropdownMenu = () => {
       </div>
 
       <div className="z-30 flex flex-grow items-center justify-start">
-        <div onClick={() => setIsOpen(!isOpen)}>
+        <button onClick={handleClickClose}>
           <img
             src={isOpen ? MENU_ICON_CLOSE : MENU_ICON}
             alt="Menu"
             className="mr-4 cursor-pointer md:mr-6 lg:hidden"
           />
-        </div>
+        </button>
 
         <img src={LOGO} alt="E-commerce sneakers" />
       </div>
