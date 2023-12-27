2f2943a7dbfc2ff043071a8b421acf3687282c88
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2023-12-27 20:03:53 +0100

    modification of position for basket on mobile

--- src/views/Modals/BasketModal/BasketModal.tsx
+++ src/views/Modals/BasketModal/BasketModal.tsx
@@ -1,8 +1,8 @@
 export const BasketModal: React.FC = () => {
   return (
-    <div className="absolute top-28 z-20 h-64 w-[360px] rounded-lg bg-white p-6 md:top-20 shadow-2xl">
+    <div className="absolute left-1/2 top-28 z-20 h-64 w-[360px] translate-x-[-50%] rounded-lg bg-white p-6 shadow-2xl md:left-auto md:top-20 md:translate-x-0">
       <p className="font-bold">Cart</p>
-      <div className=" absolute left-0 top-16 w-full border-b-[1px]"></div>
+      <div className="absolute left-0 top-16 w-full border-b-[1px]"></div>
       <p className="absolute left-1/2 top-36 translate-x-[-50%] text-center font-bold text-paleSky">
         Your cart is empty.
       </p>
