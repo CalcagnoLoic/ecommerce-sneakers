8de32ad392aeb717d5ca7fd81b430eabf704d59f
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-01-13 12:10:24 +0100

    restructuring folders

--- src/Pages/ProductPage/index.tsx
+++ src/Pages/ProductPage/index.tsx
@@ -8,10 +8,10 @@ import Header from "../../components/Header";
 import PriceReduction from "../../components/Product/PriceReduction";
 import PriceWrapper from "../../Layout/PriceWrapper";
 import ProductDescription from "../../components/Product/ProductDescription";
+import ProductOverview from "../../components/Product/ProductOverview";
 import ProductPrice from "../../components/Product/ProductPrice";
 import ProductTitle from "../../components/Product/ProductTitle";
 import Wrapper from "../../Layout/ProductDescriptionWrapper";
-
 
 function Page() {
   return (
@@ -20,31 +20,33 @@ function Page() {
       <div className="main-content-wrapper">
         <div className="product-wrapper">
           <div className="flex-col">
-            <Wrapper>
-              <CompanyName
-                tag="h2"
-                text="sneakers company"
-                classname="company-name-text"
-              />
-              <ProductTitle name="fall limited edition sneakers" />
-              <ProductDescription name={description} />
-
-              <PriceWrapper>
-                <div className="flex">
-                  <ProductPrice price={"$125.00"} />
-
-                  <PriceReduction name="50%" />
-                </div>
-                <ProductPrice isOnSale={true} price={"$250.00"} />
-              </PriceWrapper>
-
-              <div className="mt-8 flex flex-col lg:flex-row">
-                <Counter />
-
-                <ButtonAddToCart />
-              </div>
-            </Wrapper>
+            <ProductOverview />
           </div>
+
+          <Wrapper>
+            <CompanyName
+              tag="h2"
+              text="sneakers company"
+              classname="company-name-text"
+            />
+            <ProductTitle name="fall limited edition sneakers" />
+            <ProductDescription name={description} />
+
+            <PriceWrapper>
+              <div className="flex">
+                <ProductPrice price={"$125.00"} />
+
+                <PriceReduction name="50%" />
+              </div>
+              <ProductPrice isOnSale={true} price={"$250.00"} />
+            </PriceWrapper>
+
+            <div className="mt-8 flex flex-col lg:flex-row">
+              <Counter />
+
+              <ButtonAddToCart />
+            </div>
+          </Wrapper>
         </div>
       </div>
       <Footer />
